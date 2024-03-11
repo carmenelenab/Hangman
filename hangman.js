96047df40269640c11b89717115a97274050c27c
Carmen-Elena Bruma <160878690+carmenelenab@users.noreply.github.com>
2024-03-11 20:49:01 +0100

    Add files via upload

---
+++ hangman.js
@@ -0,0 +1,38 @@
+let wordsList = ['ant', 'butterfly', 'bee', 'millipede', 'grasshopper'];
+let lives = 7;
+let choosenIndex = Math.floor(Math.random() * wordsList.length);
+let choosenWord = wordsList[choosenIndex];
+let wordLen = choosenWord.length;
+let element = document.getElementById('word');
+
+document.getElementById('lettersNo').textContent
+    = "You have to guess " + wordLen + " letters";
+for (let i = 0; i < wordLen; ++i) {
+    element.textContent = element.textContent + '_ ';
+}
+
+document.getElementById('check').onclick = function() {
+    let found = false;
+    let searchedLetter = document.getElementById('letter').value.trim().toLowerCase();
+    for (let i = 0; i < choosenWord.length && wordLen > 0; ++i) {
+        if (choosenWord[i] === searchedLetter) {
+            let wordArray = element.textContent.split(' ');
+            element.textContent = element.textContent + searchedLetter;
+            wordArray[i] = searchedLetter;
+            element.textContent = wordArray.join(' ');
+            --wordLen;
+            found = true;
+        } 
+    }
+    document.getElementById('letter').value = '';
+    if (found === false && lives > 0) {
+        --lives;
+        document.getElementById('livesNo').textContent 
+            ="You have " + lives + " lives left";
+    }
+    if (!lives) {
+        document.getElementById('result').textContent = "Sorry, you lost";
+    } else if (!wordLen) {
+        document.getElementById('result').textContent = "Congrats, you won!";
+    }
+}
