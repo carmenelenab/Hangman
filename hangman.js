355f483b45b5f8f7c4fd37ad315c0e4747c7c225
carmenelenab <bruma.carmenelena@yahoo.com>
2024-03-22 18:20:29 +0100

    create new functions

--- hangman.js
+++ hangman.js
@@ -1,18 +1,24 @@
 let wordsList = ['ant', 'butterfly', 'bee', 'millipede', 'grasshopper'];
 let lives = 7;
+let found = false;
 let choosenIndex = Math.floor(Math.random() * wordsList.length);
 let choosenWord = wordsList[choosenIndex];
 let wordLen = choosenWord.length;
 let element = document.getElementById('word');
 
-document.getElementById('lettersNo').textContent
-    = "You have to guess " + wordLen + " letters";
-for (let i = 0; i < wordLen; ++i) {
-    element.textContent = element.textContent + '_ ';
+window.onload = function () {
+    startGame();
 }
 
-document.getElementById('check').onclick = function() {
-    let found = false;
+function startGame() {
+    document.getElementById('lettersNo').textContent
+        = "You have to guess " + wordLen + " letters";
+    for (let i = 0; i < wordLen; ++i) {
+        element.textContent = element.textContent + '_ ';
+    }
+}
+
+function checkLetterMatch() {
     let searchedLetter = document.getElementById('letter').value.trim().toLowerCase();
     for (let i = 0; i < choosenWord.length && wordLen > 0; ++i) {
         if (choosenWord[i] === searchedLetter) {
@@ -22,14 +28,21 @@ document.getElementById('check').onclick = function() {
             element.textContent = wordArray.join(' ');
             --wordLen;
             found = true;
-        } 
+        }
     }
     document.getElementById('letter').value = '';
+    livesLeft();
+    endOfTheGame();
+}
+
+function livesLeft() {
     if (found === false && lives > 0) {
         --lives;
-        document.getElementById('livesNo').textContent 
-            ="You have " + lives + " lives left";
+        document.getElementById('livesNo').textContent = "You have " + lives + " lives left";
     }
+}
+
+function endOfTheGame() {
     if (!lives) {
         document.getElementById('result').textContent = "Sorry, you lost";
     } else if (!wordLen) {
